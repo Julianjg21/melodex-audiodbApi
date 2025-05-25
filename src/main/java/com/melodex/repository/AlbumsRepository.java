2ed73bcf1a67864648bba18d60812a33544fce23
Julian Jimenez <julianjimenez2128@gmail.com>
2025-05-25 01:43:22 -0500

    chore: remove unused import statement from AlbumsRepository

--- src/main/java/com/melodex/repository/AlbumsRepository.java
+++ src/main/java/com/melodex/repository/AlbumsRepository.java
@@ -5,12 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
+
 import java.util.Optional;
 
 public interface AlbumsRepository extends JpaRepository<Album, Integer> {
 
-   
+
     @Query("SELECT al.idAlbum FROM Album al WHERE al.albumName = :name")
     Optional<Long> findIdByAlbumName(@Param("name") String albumName);
 
